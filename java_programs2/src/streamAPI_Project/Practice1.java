82c15eb53eb3100b8cbb1739081a8654262f6ab4
Mr.Pool <129317078+nikhilajjarapu6@users.noreply.github.com>
2025-04-29 11:34:13 +0530

    Delete java_programs2 directory

--- java_programs2/src/streamAPI_Project/Practice1.java
+++
@@ -1,9 +0,0 @@
-package streamAPI_Project;
-
-import java.util.List;
-
-public class Practice1 {
-	public static void main(String[] args) {
-		
-	}
-}
--- java_programs2/src/strings/UpperCase.java
+++
@@ -1,9 +0,0 @@
-package strings;
-
-public class UpperCase {
-	public static void main(String[] args) {
-		String str="hello world java";
-		System.out.println(str.toUpperCase());
-	}
-
-}
